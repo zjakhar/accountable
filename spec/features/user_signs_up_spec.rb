e82f74f0759a65eacf54d42eb039b478ede70d1c
Zain Jakhar <zainjakhar@gmail.com>
2020-02-10 14:25:37 -0500

    added first and last name to signup testing

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -6,15 +6,11 @@ feature 'user registers', %Q{
   So that I can create an account
 } do
 
-  # Acceptance Criteria:
-  # * I must specify a valid email address,
-  #   password, and password confirmation
-  # * If I don't specify the required information, I am presented with
-  #   an error message
-
   scenario 'provide valid registration information' do
     visit new_user_registration_path
 
+    fill_in 'First Name', with: "Zain"
+    fill_in 'Last Name', with: "Jakhar"
     fill_in 'Email', with: 'john@example.com'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
