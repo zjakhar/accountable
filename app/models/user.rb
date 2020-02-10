4eca4e764c96947ea74fadbc7d55ca42f9cd775b
Zain Jakhar <zainjakhar@gmail.com>
2020-02-10 16:20:24 -0500

    added mount uploader for profile picture to our User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  mount_uploader :profile_photo, ProfilePhotoUploader
 end
