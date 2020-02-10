8653f5924490a368830905d267dd338cb74c5794
Zain Jakhar <zainjakhar@gmail.com>
2020-02-10 14:41:32 -0500

    added model testing for User, check the first name, last name, email, and password.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+
+  it { should have_valid(:first_name).when("Zain") }
+  it { should_not have_valid(:first_name).when(nil, "") }
+
+  it { should have_valid(:last_name).when("Doe") }
+  it { should_not have_valid(:last_name).when(nil, "") }
+
+  it { should have_valid(:email).when("zain@email.com") }
+  it { should_not have_valid(:email).when(nil, "", "zain@", "zain", "zain.com") }
+
+  it { should have_valid(:encrypted_password).when("password", "123456", "passwordisverylong123") }
+  it { should_not have_valid(:encrypted_password).when(nil, "", "short") }
+
 end
