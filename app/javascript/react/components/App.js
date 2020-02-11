553e33b2ff70e722edcfaa6a1c91691420b664cb
Zain Jakhar <zainjakhar@gmail.com>
2020-02-11 13:59:35 -0500

    BrowserRouter to and Routes

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import AccountableIndex from './AccountableIndex'
 
 export const App = (props) => {
-  return (<h1>Make It So React</h1>)
-}
+return (
+  <div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={AccountableIndex} />
+      </Switch>
+    </BrowserRouter>
+  </div>
+)}
 
 export default App
