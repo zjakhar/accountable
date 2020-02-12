433fe7567643cc3e2e79d5f863d214d6e30dd8fe
Zain Jakhar <zainjakhar@gmail.com>
2020-02-12 07:28:06 -0500

    basic structure for month page

---
+++ app/javascript/react/components/MonthsIndex.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const MonthsIndex = (props) => {
+  return (
+    <div className="test">
+      <h3>Months index Jan - Dec</h3>
+      <div class="Grid">
+        <div class="Grid-item">January</div>
+        <div class="Grid-item"></div>
+        <div class="Grid-item"></div>
+        <div class="Grid-item"></div>
+        <div class="Grid-item"></div>
+        <div class="Grid-item"></div>
+        <div class="Grid-item"></div>
+        <div class="Grid-item">+</div>
+      </div>
+    </div>
+  )
+}
+
+export default MonthsIndex
