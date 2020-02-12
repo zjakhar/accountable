f31e686e7537666b82e1746d1caca2ed7b92c8ab
Zain Jakhar <zainjakhar@gmail.com>
2020-02-12 14:20:14 -0500

    added useEffect to pull any current months already made/ made monthTile

--- app/javascript/react/components/MonthsIndex.js
+++ app/javascript/react/components/MonthsIndex.js
@@ -1,19 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import MonthTile from './MonthTile'
+import { Link } from 'react-router-dom'
 
 const MonthsIndex = (props) => {
+  const [months, setMonths] = useState([])
+
+  useEffect(() => {
+    fetch('/api/v1/months.json')
+      .then(response => {
+        if(response.ok) {
+          return response
+        } else {
+          throw new Error(`${response.status}: ${response.statusText}`)
+        }
+      })
+      .then(response => response.json())
+      .then(body => {
+        setMonths(body)
+      })
+      .catch(error => {
+        console.log(`Error fetching albums: ${error.message}`)
+      })
+  }, [])
+
+  const monthTiles = months.map((month) => {
+    return (
+      <MonthTile
+        key={month.id}
+        monthData= {month}
+      />
+    )
+  })
+
   return (
     <div className="test">
-      <h3>Months index Jan - Dec</h3>
-      <div class="Grid">
-        <div class="Grid-item">January</div>
-        <div class="Grid-item"></div>
-        <div class="Grid-item"></div>
-        <div class="Grid-item"></div>
-        <div class="Grid-item"></div>
-        <div class="Grid-item"></div>
-        <div class="Grid-item"></div>
-        <div class="Grid-item">+</div>
-      </div>
+      <span>
+        { monthTiles }
+      </span>
+      <span className="Grid">
+        <Link to={`/months/new`} className="Grid-item">
+          <div className="month-text-last">
+            + Add New Month +
+          </div>
+        </Link>
+      </span>
     </div>
   )
 }
