5d4531539f698912133fc8e285d06cfe331d4343
Zain Jakhar <zainjakhar@gmail.com>
2020-02-12 07:28:44 -0500

    controller work for months controller

---
+++ app/controllers/api/v1/months_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::MonthsController < ApplicationController
+  # before_action :authenticate_user!, only: [:index]
+  def index
+    render json: Month.all
+  end
+
+  def create
+    month = Month.new(month_params)
+    if month.save
+      render json { month: month }
+    else
+      render json: { error: album.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+private
+
+  def month_params
+    params.permit(:month)
+  end
+
+end
