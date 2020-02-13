9e9cd75aaf4d6c5d7ae380f65f24abf7740db636
Zain Jakhar <zainjakhar@gmail.com>
2020-02-13 13:11:44 -0500

    added create to route for user to add a new month

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :months, only: [:index]
+      resources :months, only: [:index, :create]
     end
   end
 end
