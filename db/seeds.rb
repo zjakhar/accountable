a87496781ca6a7d97df5b93631f40ed64a4bf05b
Zain Jakhar <zainjakhar@gmail.com>
2020-02-15 18:03:09 -0500

    seed use for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Month.create(month:"January")
+
+Lineitem.create!(month_id: 9, user_id: 7, category: "income", lineitem: "check", value: 4000)
+Lineitem.create!(month_id: 9, user_id: 7, category: "expense", lineitem: "bill", value: 200)
+
+Lineitem.create!(month_id: 10, user_id: 7, category: "income", lineitem: "check", value: 4000)
+Lineitem.create!(month_id: 10, user_id: 7, category: "expense", lineitem: "bill", value: 300)
+
+Lineitem.create!(month_id: 11, user_id: 7, category: "income", lineitem: "check", value: 4000)
+Lineitem.create!(month_id: 11, user_id: 7, category: "expense", lineitem: "bill", value: 400)
