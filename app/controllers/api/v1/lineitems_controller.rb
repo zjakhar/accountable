67bcc3a12a4107c3389c7d2dc749ea03b2a27c6d
Zain Jakhar <zainjakhar@gmail.com>
2020-02-15 17:57:35 -0500

    added controller for lineitems

---
+++ app/controllers/api/v1/lineitems_controller.rb
@@ -0,0 +1,34 @@
+class Api::V1::LineitemsController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :show, :create]
+
+  def index
+    # binding.pry
+    render json: Lineitem.all
+  end
+
+  def show
+    # binding.pry
+    lineitem = Lineitem.find(params[:id])
+    render json: lineitem
+  end
+
+  def create
+    month = Month.find(params["month_id"])
+    lineitem = Lineitem.new(lineitem_params)
+    lineitem.user = current_user
+    lineitem.month = month
+    if lineitem.save
+        render json: lineitem
+    else
+     render json: { error: review.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+
+private
+
+def lineitem_params
+  params.permit(:month_id, :user_id, :category, :lineitem, :value)
+end
+
+end
