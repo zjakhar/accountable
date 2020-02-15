56763e960e3d4aa87109ca3852206eba8f62552c
Zain Jakhar <zainjakhar@gmail.com>
2020-02-15 18:02:46 -0500

    routes for lineitems and show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   get "/months", to: 'homes#index'
+  get "/months/:id", to: 'homes#index'
 
   namespace :api do
     namespace :v1 do
-      resources :months, only: [:index, :create]
+      resources :months, only: [:index, :show, :create] do
+        resources :lineitems, only: [:index, :show, :create]
+      end
     end
   end
 end
