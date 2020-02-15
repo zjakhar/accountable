b1c6e7089aa130477d041d90169a350def8fd409
Zain Jakhar <zainjakhar@gmail.com>
2020-02-15 17:59:31 -0500

    month show page

---
+++ app/javascript/react/components/MonthShow.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import LineitemTile from './LineitemTile'
+
+const MonthShow = (props) => {
+
+  return(
+    <div>
+      <h1>Start Your Budget for this month </h1>
+      
+    </div>
+  )
+}
+
+export default MonthShow
