a70576ea1b575c41ad4571eb315b70eca102e057
Zain Jakhar <zainjakhar@gmail.com>
2020-02-16 12:44:31 -0500

    added classname and dynamic month

--- app/javascript/react/components/MonthShow.js
+++ app/javascript/react/components/MonthShow.js
@@ -2,11 +2,9 @@ import React from 'react'
 import LineitemTile from './LineitemTile'
 
 const MonthShow = (props) => {
-
   return(
     <div>
-      <h1>Start Your Budget for this month </h1>
-      
+      <h1 className="month-title">Your Budget for {props.month.month}</h1>
     </div>
   )
 }
