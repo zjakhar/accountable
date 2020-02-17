7b8ff2cabf3f82b744dd91d83b07b2002b469b80
Zain Jakhar <zainjakhar@gmail.com>
2020-02-16 23:35:25 -0500

    added Routes for about and contact

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import AccountableIndex from './AccountableIndex'
 import MonthsIndexContainer from './MonthsIndexContainer'
 import MonthShowContainer from './MonthShowContainer'
+import About from './About'
+import Contact from './Contact'
 
 export const App = (props) => {
 return (
@@ -12,6 +14,8 @@ return (
         <Route exact path="/" component={ AccountableIndex } />
         <Route exact path="/months" component={ MonthsIndexContainer } />
         <Route exact path="/months/:id" component={ MonthShowContainer } />
+        <Route exact path="/about" component={ About } />
+        <Route exact path="/contact" component={ Contact } />
       </Switch>
     </BrowserRouter>
   </div>
