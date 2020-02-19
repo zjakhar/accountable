7acf6cc3e054e504814f12a86d7c55110b874e5c
Anum Mirza <anummirza@Anums-MacBook-Pro.local>
2020-02-19 14:42:19 -0500

    path for edit

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import AccountableIndex from './AccountableIndex'
 import MonthsIndexContainer from './MonthsIndexContainer'
 import MonthShowContainer from './MonthShowContainer'
+import EditMonthForm from './EditMonthForm'
 import About from './About'
 import Contact from './Contact'
 
@@ -13,9 +14,10 @@ return (
       <Switch>
         <Route exact path="/" component={ AccountableIndex } />
         <Route exact path="/months" component={ MonthsIndexContainer } />
-        <Route exact path="/months/:id" component={ MonthShowContainer } />
         <Route exact path="/about" component={ About } />
         <Route exact path="/contact" component={ Contact } />
+        <Route exact path="/months/:id" component={ MonthShowContainer } />
+        <Route exact path="/month/:id/edit" component={EditMonthForm} />
       </Switch>
     </BrowserRouter>
   </div>
