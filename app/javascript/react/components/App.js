f6c517492107600ca06d68ded5419803479f0042
Anum Mirza <anummirza@Anums-MacBook-Pro.local>
2020-02-20 14:54:14 -0500

    routes

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -3,19 +3,21 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import AccountableIndex from './AccountableIndex'
 import MonthsIndexContainer from './MonthsIndexContainer'
 import MonthShowContainer from './MonthShowContainer'
+import MapWithASearchBox from './MapWithASearchBox'
 import EditMonthForm from './EditMonthForm'
-import About from './About'
 import Contact from './Contact'
+import About from './About'
 
 export const App = (props) => {
 return (
-  <div>
+  <div className='test'>
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ AccountableIndex } />
         <Route exact path="/months" component={ MonthsIndexContainer } />
         <Route exact path="/about" component={ About } />
         <Route exact path="/contact" component={ Contact } />
+        <Route exact path="/advisors" component={ MapWithASearchBox } />
         <Route exact path="/months/:id" component={ MonthShowContainer } />
         <Route exact path="/month/:id/edit" component={EditMonthForm} />
       </Switch>
