2fef37d07b13b95d3b0d0062800989fed9c6fdac
Anum Mirza <anummirza@Anums-MacBook-Pro.local>
2020-02-21 16:11:27 -0500

    removed contact routes

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -5,7 +5,6 @@ import MonthsIndexContainer from './MonthsIndexContainer'
 import MonthShowContainer from './MonthShowContainer'
 import MapWithASearchBox from './MapWithASearchBox'
 import EditMonthForm from './EditMonthForm'
-import Contact from './Contact'
 import About from './About'
 
 export const App = (props) => {
@@ -16,7 +15,6 @@ return (
         <Route exact path="/" component={ AccountableIndex } />
         <Route exact path="/months" component={ MonthsIndexContainer } />
         <Route exact path="/about" component={ About } />
-        <Route exact path="/contact" component={ Contact } />
         <Route exact path="/advisors" component={ MapWithASearchBox } />
         <Route exact path="/months/:id" component={ MonthShowContainer } />
         <Route exact path="/month/:id/edit" component={EditMonthForm} />
