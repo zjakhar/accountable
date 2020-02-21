d8674bd385980a44b37ae59447891464d088293a
Anum Mirza <anummirza@Anums-MacBook-Pro.local>
2020-02-21 11:57:55 -0500

    removed filler text

--- app/javascript/react/components/AccountableIndex.js
+++ app/javascript/react/components/AccountableIndex.js
@@ -12,7 +12,7 @@ const AccountableIndex = (props) => {
               <header className="major">
                 <h2>Take Control of Your Future</h2>
               </header>
-              <p className="welcome">Give Every $ a Purpose</p><p className="welcome">Eliminate Debt</p><p className="welcome">Reduce Stress</p>
+              <p className="welcome">Give Every Dollar a Purpose</p><p className="welcome">Eliminate Debt</p><p className="welcome">Reduce Stress</p>
               <a href="/users/sign_up" className="button">Join for Free</a>
             </div>
           </div>
@@ -27,7 +27,20 @@ const AccountableIndex = (props) => {
               <header className="major">
                 <h2>Create Monthly Budgets to Help Take Back Control of Your Income</h2>
               </header>
-              <p className="welcome">Sed nunc dui, laoreet et fringilla nec, aliquam sit amet dolor. Cras faucibus, justo vel ullamcorper accumsan, elit nibh tempus dui, eget tincidunt ante dolor eget nisi. Morbi at viverra eros. Nulla sed dui dui. Ut gravida euismod dolor vitae.</p>
+              <p className="welcome">The simple act of budgeting and determining your monies uses affects human behavior. Labeling our money’s uses, and the goods we purchase as an appropriate use for that money, leads to changed behavior. In the case of a budget, assigning our money leads to living within our means and saving money.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="special" data-scrollax-parent="true">
+        <div className="cover" data-scrollax="properties: { translateY: '30%' }"><img src="https://images.unsplash.com/photo-1502920514313-52581002a659?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80" /></div>
+        <div className="content">
+          <div className="text centralize">
+            <div data-scrollax="properties: { 'translateY': '150%', 'opacity': 1.5 }">
+              <header className="cell small-6 major">
+                <h2 className="">No Longer Live With unnecessary Stress</h2>
+              </header>
+              <p className="welcome">Make a plan for your income. Accountable provides you with a template for budgeting your monthly expenses. When you know where you are spending your money, you will easily find ways to cut out unnecessary spending.</p>
             </div>
           </div>
         </div>
@@ -38,35 +51,10 @@ const AccountableIndex = (props) => {
           <div className="text centralize">
             <div data-scrollax="properties: { 'translateY': '150%', 'opacity': 1.5 }">
               <header className="major">
-                <h2>No Longer Live With unnecessary Stress</h2>
-              </header>
-              <p className="welcome">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent est turpis, aliquam quis molestie in, imperdiet a urna. Maecenas vel condimentum enim, eget tempor lectus. Vivamus ut nisi in enim vestibulum aliquam at quis nunc. Suspendisse ac erat eu.</p>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="special" data-scrollax-parent="true">
-        <div className="cover" data-scrollax="properties: { translateY: '30%' }"><img src="https://images.unsplash.com/photo-1502920514313-52581002a659?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80" /></div>
-        <div className="content">
-          <div className="text centralize">
-            <div data-scrollax="properties: { 'translateY': '150%', 'opacity': 1.5 }">
-              <header className="major">
-                <h2>Hear What Other using Accountable are Doing to Save More Money</h2>
-              </header>
-              <p className="welcome">Sed eget arcu eget risus semper placerat. Aliquam aliquet erat augue, non scelerisque augue rhoncus vitae.</p>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="special" data-scrollax-parent="true">
-        <div className="cover" data-scrollax="properties: { translateY: '30%' }"><img src="https://images.unsplash.com/photo-1569073120512-05362a6b92e7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" /></div>
-        <div className="content">
-          <div className="text centralize">
-            <div data-scrollax="properties: { 'translateY': '150%', 'opacity': 1.5 }">
-              <header className="major">
                 <h2>Find Financial Planners Close to You</h2>
               </header>
-              <p className="welcome">Quisque interdum convallis lacus ut auctor. Nunc venenatis elit non nulla gravida, congue fringilla justo ultrices. Morbi ut dolor fringilla, mollis ante imperdiet, euismod sem.</p>
+              <p className="welcome">Use our Google Maps feature to find Financial Planners in the city you live in.</p>
+              <a href="/users/sign_up" className="button">Join for Free</a>
             </div>
           </div>
         </div>
