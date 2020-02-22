a60cf32cf332b7b65e0b1643e26821cae69578cb
Anum Mirza <anummirza@Anums-MacBook-Pro.local>
2020-02-22 01:27:45 -0500

    chart edits

--- app/javascript/react/components/Charts.js
+++ app/javascript/react/components/Charts.js
@@ -2,25 +2,25 @@ import React from 'react';
 import { Chart } from "react-google-charts";
 
 const Charts = (props) => {
-  let array = props.lineitems
-  let emptyArray = [['LineItem', 'Value']]
-    let chartData = array.map((item) =>{
+  let lineitemArray = props.lineitems
+  let array = [['LineItem', 'Value']]
+    let chartData = lineitemArray.map((item) =>{
       if(item.category === "Expense") {
-      emptyArray.push([item.lineitem, item.value])
+      array.push([item.lineitem, item.value])
       }
     })
 
   return (
     <div className = "column chart">
       <Chart
-        width={'500px'}
-        height={'300px'}
+        width={'600px'}
+        height={'400px'}
         size={'90px'}
         chartType="PieChart"
         loader={<div>Loading Chart</div>}
-        data={emptyArray}
+        data={array}
         options={{
-          title: `${props.month.month} expenses, you have $${props.remainingToBudget(props.lineitems)} left to budget to reach a zero based budget!`,
+          title: `Expense Chart: You have $${props.remainingToBudget(props.lineitems)} left to budget to reach a zero based budget!`,
           is3D: true,
           backgroundColor:'transparent'
         }}
