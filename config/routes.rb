5e6d81474f8d60fc8b34f9588a254a94d3fbc3ea
Anum Mirza <anummirza@Anums-MacBook-Pro.local>
2020-02-23 15:44:58 -0500

    added destroy for months

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :months, only: [:index, :show, :create, :update] do
+      resources :months, only: [:index, :show, :create, :update, :destroy] do
         resources :lineitems, only: [:index, :show, :create, :destroy]
       end
     end
